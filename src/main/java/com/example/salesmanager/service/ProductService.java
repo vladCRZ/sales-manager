69b274e16a60d9065881e627463ce8c19dfc8d4e
Sergio Cruz <vlad.crz@gmail.com>
2023-05-27 02:06:08 -0600

    Adding sorting and paging.

--- src/main/java/com/example/salesmanager/service/ProductService.java
+++ src/main/java/com/example/salesmanager/service/ProductService.java
@@ -5,6 +5,10 @@ import java.util.List;
 import com.example.salesmanager.entity.Product;
 import com.example.salesmanager.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -15,8 +19,14 @@ public class ProductService {
     @Autowired
     private ProductRepository repo;
 
-    public List<Product> listAll() {
-        return repo.findAll();
+    public Page<Product> listAll(int pageNum, String sortField, String sortDir) {
+        int pageSize = 5;
+        Pageable pageable = PageRequest.of(pageNum - 1, pageSize,
+                sortDir.equals("asc") ? Sort.by(sortField).ascending()
+                        : Sort.by(sortField).descending()
+        );
+
+        return repo.findAll(pageable);
     }
 
     public void save(Product product) {
